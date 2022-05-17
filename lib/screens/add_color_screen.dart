540ec30a26494a0070d989581a1930a5ba8e6519
yplog <yalinpala@protonmail.com>
2022-05-17 10:15:37 +0300

    add_color_screen textfield

--- lib/screens/add_color_screen.dart
+++ lib/screens/add_color_screen.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
-class AddColorScreen extends StatelessWidget {
+class AddColorScreen extends StatefulWidget {
   const AddColorScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AddColorScreen> createState() => _AddColorScreenState();
+}
+
+class _AddColorScreenState extends State<AddColorScreen> {
+  TextEditingController noteController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -10,13 +17,23 @@ class AddColorScreen extends StatelessWidget {
         title: const Text('Add Color Screen'),
       ),
       backgroundColor: const Color(0xffffffff),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            debugPrint('ihihi');
-          },
-          child: const Text('Go back!'),
+      body: Container(margin: const EdgeInsets.all(20), child: Center(
+        child: TextField(
+          textAlign: TextAlign.start,
+          maxLength: 40,
+          controller: noteController,
+          decoration: const InputDecoration(
+              hintText: 'Enter your note',
+          ),
         ),
+      ),),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print(noteController.text);
+        },
+        tooltip: 'Add new color',
+        splashColor: Colors.deepPurple,
+        child: const Icon(Icons.add),
       ),
     );
   }
